53a64b3c374df6c9db2dfdbc6bbb4787e7623567
Sohardbegraj <sohard16begraj@gmail.com>
2025-07-29 19:44:35 +0530

    intialisation

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/yourname/backend
+
+go 1.24.5
---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+  fetch('/api')
+    .then(res => res.text())
+    .then(console.log);
+}, []);
+
+  return (
+    <>
+      <h1>Hello from React frontend</h1>
+
+    </>
+  )
+}
+
+export default App
